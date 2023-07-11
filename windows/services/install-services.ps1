13436732d03f2dcef07ca48c70f9a8b8ab18db26
Jake Levitt <jake.levitt@olo.com>
2023-07-11 14:02:21 -0400

    Add services for focal tunnels.

--- windows/services/install-services.ps1
+++ windows/services/install-services.ps1
@@ -10,11 +10,17 @@ gsv Omniprox | sasv
 winsw install $PSScriptRoot\MySqlDevTunnel\MySqlDevTunnel.xml
 gsv MySqlDevTunnel | sasv
 
+winsw install $PSScriptRoot\MySqlDevTunnel-Focal\MySqlDevTunnel-Focal.xml
+gsv MySqlDevTunnelFocal | sasv
+
 winsw install $PSScriptRoot\MySqlDevCloudPOSTunnel\MySqlDevCloudPOSTunnel.xml
 gsv MySqlDevCloudPOSTunnel | sasv
 
 winsw install $PSScriptRoot\SFTPTunnel\SFTPTunnel.xml
 gsv SFTPTunnel | sasv
+
+winsw install $PSScriptRoot\SFTPTunnel-Focal\SFTPTunnel-Focal.xml
+gsv SFTPTunnelFocal | sasv
 
 winsw install $PSScriptRoot\RabbitMQAdminTunnel\RabbitMQAdminTunnel.xml
 gsv RabbitMQAdminTunnel | sasv
