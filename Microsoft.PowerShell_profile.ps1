552810aafcc0cb92bf95a42c56db0af652e3a45c
Jake Levitt <jake.levitt@olo.com>
2023-07-13 15:41:56 -0400

    Add Invoke-WebRequest wrapper that extracts the response body as a string.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -719,3 +719,8 @@ function List-COMComponents
 {
     gci HKLM:\Software\Classes -ea 0| ? {$_.PSChildName -match '^\w+\.\w+$' -and (gp "$($_.PSPath)\CLSID" -ea 0)} | ft PSChildName
 }
+
+function ivr
+{
+    [System.Text.Encoding]::UTF8.GetString((Invoke-WebRequest @args).Content)
+}
