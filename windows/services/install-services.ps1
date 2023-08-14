ca9b674a1ab277f8eba95eb5e264564ec051eedc
Jake Levitt <jake.levitt@olo.com>
2023-08-14 09:56:47 -0400

    Take WinSW service creds from env vars.

--- windows/services/install-services.ps1
+++ windows/services/install-services.ps1
@@ -1,30 +1,24 @@
-#TODO: Use Get-Credential to get once
-#$c = Get-Credential "jlevitt"
-#$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($c.Password)
-#$UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
-#$UnsecurePassword
-
-winsw install $PSScriptRoot\Omniprox\Omniprox.xml
+winsw install $PSScriptRoot\Omniprox\Omniprox.xml --user $env:WINSW_USERNAME --password $env:WINSW_PASSWORD
 gsv Omniprox | sasv
 
-winsw install $PSScriptRoot\MySqlDevTunnel\MySqlDevTunnel.xml
+winsw install $PSScriptRoot\MySqlDevTunnel\MySqlDevTunnel.xml --user $env:WINSW_USERNAME --password $env:WINSW_PASSWORD
 gsv MySqlDevTunnel | sasv
 
-winsw install $PSScriptRoot\MySqlDevTunnel-Focal\MySqlDevTunnel-Focal.xml
+winsw install $PSScriptRoot\MySqlDevTunnel-Focal\MySqlDevTunnel-Focal.xml --user $env:WINSW_USERNAME --password $env:WINSW_PASSWORD
 gsv MySqlDevTunnelFocal | sasv
 
-winsw install $PSScriptRoot\MySqlDevCloudPOSTunnel\MySqlDevCloudPOSTunnel.xml
+winsw install $PSScriptRoot\MySqlDevCloudPOSTunnel\MySqlDevCloudPOSTunnel.xml --user $env:WINSW_USERNAME --password $env:WINSW_PASSWORD
 gsv MySqlDevCloudPOSTunnel | sasv
 
-winsw install $PSScriptRoot\SFTPTunnel\SFTPTunnel.xml
+winsw install $PSScriptRoot\SFTPTunnel\SFTPTunnel.xml --user $env:WINSW_USERNAME --password $env:WINSW_PASSWORD
 gsv SFTPTunnel | sasv
 
-winsw install $PSScriptRoot\SFTPTunnel-Focal\SFTPTunnel-Focal.xml
+winsw install $PSScriptRoot\SFTPTunnel-Focal\SFTPTunnel-Focal.xml --user $env:WINSW_USERNAME --password $env:WINSW_PASSWORD
 gsv SFTPTunnelFocal | sasv
 
-winsw install $PSScriptRoot\RabbitMQAdminTunnel\RabbitMQAdminTunnel.xml
+winsw install $PSScriptRoot\RabbitMQAdminTunnel\RabbitMQAdminTunnel.xml --user $env:WINSW_USERNAME --password $env:WINSW_PASSWORD
 gsv RabbitMQAdminTunnel | sasv
 
-winsw install $PSScriptRoot\DashDevTunnel\DashDevTunnel.xml
+winsw install $PSScriptRoot\DashDevTunnel\DashDevTunnel.xml --user $env:WINSW_USERNAME --password $env:WINSW_PASSWORD
 gsv DashDevTunnel | sasv
 
